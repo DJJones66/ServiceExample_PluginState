05261c0507a334c1e989bbf4638a33b6ef709914
David Jones <david-j-jones@hotmail.com>
2025-07-23 17:12:32 -0400

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ module.exports = {
   mode: "development",
   entry: "./src/index",
   output: {
-    path: path.resolve(__dirname, '/home/hacker/BrainDriveDev/BrainDrive/backend/plugins/shared/ServiceExample_PluginState/v1.0.0/dist'),
-    //path: path.resolve(__dirname, 'dist'),
+    //path: path.resolve(__dirname, '/Your BrainDrive Dir/BrainDrive/backend/plugins/shared/ServiceExample_PluginState/v1.0.0/dist'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: "auto",
     clean: true,
     library: {
